473605156df181ca96de8ff675c9f6e7d5f20dd9
github username <github email address>
2018-09-18 13:11:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,17 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    #Instance variables in a controller method allow the contents to be visible to the erb file which it renders.
+    # instance variables cannot be passed between controller methods.
     erb :reversed
   end
 
   get '/friends' do
-    # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
+    erb :friends
   end
+  
 end
